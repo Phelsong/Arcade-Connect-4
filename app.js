c8142f2e8a0b253fee088ef74d7cb8c5728a3a43
Josh S Wilkinson <joshswilkinson@outlook.com>
2022-03-02 11:33:43 -0600

    finished basic Board generation

--- app.js
+++ app.js
@@ -1,20 +1,73 @@
-const gameBoard = {}
+/* Variable naming key
+html'xxx' = static html
+vis'xxx' = procedurally gen html
+style'xxx' = CSS visual element
+*/
+const visBoard = document.querySelector('.htmlGameBoard')
+const gameState = {
+    gameBoard : {},
+    players : ['red', 'yellow'],
+    turn : null,// define value
+    gameStatus: 'playing',
+
+}
+let userBoardSize = 4
+
+function generateColumns() {
+    let map = new Map([
+        ["slot1", null],
+        ["slot2", null],
+        ["slot3", null],
+        ["slot4", null],
+        ["slot5", null],
+        ["slot6", null]
+    ]) 
+    createColumn()
+    return map 
+}
 
 function createColumn() {
-    return new Map() 
+    let visColumn = document.createElement('div')
+    visColumn.classList.add('styleColumn')
+    for (let i = 0; i < 6; i++) {
+        let visSlot = document.createElement('div')
+        visSlot.id = `slot${i+1}`
+        visSlot.classList.add('styleSlot')
+        visColumn.appendChild(visSlot)
+    }
+    visBoard.appendChild(visColumn)
 }
 
-let userBoardSize = null
 
-for (let i = 0; i < userBoardSize; i ++){
-gameBoard.set(`column` + `${i}`, createColumn)
+function setGameBoard (){
+    for (let i = 0; i < userBoardSize+3; i ++){
+    gameState.gameBoard[`column${i+1}`] = generateColumns()
+    }
+}
+setGameBoard()
+
+
+function placeGameToken () {
+    Object.getOwnPropertyDescriptor(gameState.gameBoard, "@click location@")
+    
+    Object.definteProperty(gameState, gameBoard, "@click location@", {value : "@turn@"})
+    let visToken = document.createElement('div')
+    visToken.id = `turn token`
+    visToken.classList.add('styleToken')
+    visSlot.appendChild(visToken)
 }
 
+// function solutionCheck () {
+//     for (x of gameState.gameBoard){
+//         
+//     }
+// }
 
-function gameToken () {
- // set gameBoard.columnX.value = token 
-}
+//click thee olde DOM column to drop token
 
-function updateBoard() {
-    // update board values
-}
+// function updateBoard() {
+//     // update board values
+// }
+
+
+
