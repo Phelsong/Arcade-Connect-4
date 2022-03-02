dbd53ba7d1a3cd8e1813fd116a26280de28ea87d
Josh S Wilkinson <joshswilkinson@outlook.com>
2022-03-02 08:33:22 -0600

    day 2 start point

---
+++ app.js
@@ -0,0 +1,20 @@
+const gameBoard = {}
+
+function createColumn() {
+    return new Map() 
+}
+
+let userBoardSize = null
+
+for (let i = 0; i < userBoardSize; i ++){
+gameBoard.set(`column` + `${i}`, createColumn)
+}
+
+
+function gameToken () {
+ // set gameBoard.columnX.value = token 
+}
+
+function updateBoard() {
+    // update board values
+}
