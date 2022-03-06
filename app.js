2405f0a29e58ec44c162df686ca6871e73804934
Josh S Wilkinson <joshswilkinson@outlook.com>
2022-03-06 12:18:55 -0600

    turn cycle and value assignment working

--- app.js
+++ app.js
@@ -5,89 +5,101 @@ style'xxx' = CSS visual element
 */
 const visBoard = document.querySelector('.htmlGameBoard')
 const dropSelection = document.querySelector('select')
-let chosenSize = 4
 
 const gameState = {
     gameBoard : {},
+    connect: 4,
     players : ['red', 'yellow'],
-    turn : 'red',// define value
+    turn : 'red', // define value
     gameStatus: 'playing',
+    setGameBoard: function() {
+        // sets the size of the board based on the dropdown selection
+        function generateColumnMap() {
+            let map = new Map([
+                ["slot1", null], 
+                ["slot2", null],
+                ["slot3", null],
+                ["slot4", null],
+                ["slot5", null],
+                ["slot6", null]
+            ]) 
+            return map 
+        }
+        function createHTMLColumn(counter) {
+            let column = document.createElement('div')
+            column.classList.add(`htmlColumn`, 'styleColumn')
+            column.id = `column${counter}`
+            for (let i = 1; i < 7; i++) {
+                gameState.gameBoard[`column${i}`] 
+                let slot = document.createElement('div')
+                slot.classList.add(`htmlSlot`, 'styleSlot')
+                slot.id = `column${counter}slot${i}`
+                column.appendChild(slot)
+            }
+            visBoard.appendChild(column)
+        }
+        for (let i = 1; i < this.connect+4; i ++){
+        createHTMLColumn(i)
+        this.gameBoard[`column${i}`] = generateColumnMap()
+        }
+   
+    },
+    placeGameToken: function (columnClicked) {
+    
+        //find the first unassigned slot and set its value to player turn
+        let slotId = null
+        function slotFinder () {
+            for (let i = 1; i < 7; i++) {
+       
+            let slotValue = gameState.gameBoard[columnClicked].get(`slot${i}`)
+
+                if (slotValue === null){
+                gameState.gameBoard[columnClicked].set(`slot${i}`, gameState.turn)
+                slotId = `slot${i}`
+                break;
+                }
+            }
+        }   
+        //function call
+        slotFinder()
+
+        //place visual token in the first to the same unassigned slot
+        function generateGameToken () {
+            const slot = document.querySelector(`#` +columnClicked+slotId)
+            const token = document.createElement('div')
+            token.classList.add (`playerToken`, `styleToken`)
+            slot.appendChild(token)
+        }
+        // function call
+        generateGameToken()
+        console.log(gameState.gameBoard, 'gameboard post click')
+    },
+    checkForWinner: function (){}
     
 }
 console.log(gameState.gameBoard)
 
 // **** Set Game Size ****
-
+//dropdown game-size listener
 dropSelection.addEventListener("change", function(event){
-    chosenSize = Number(event.target.value)
-    console.log(event.target.value)
-});
+    gameState.connect = Number(event.target.value)
+}),
+// function call to set game size
+gameState.setGameBoard()
 
-function setGameBoard (){
-    function generateColumns() {
-        let map = new Map([
-            ["slot1", null], //red
-            ["slot2", null],
-            ["slot3", null],
-            ["slot4", null],
-            ["slot5", null],
-            ["slot6", null]
-        ]) 
-        return map 
-    }
-    function createColumn(counter) {
-        let column = document.createElement('div')
-        column.classList.add(`htmlColumn`, 'styleColumn')
-        column.id = `column${counter+1}`
-        for (let i = 0; i < 6; i++) {
-            let slot = document.createElement('div')
-            slot.classList.add(`htmlSlot`, 'styleSlot')
-            slot.id = `column${counter+1}slot${i+1}`
-            column.appendChild(slot)
-        }
-        visBoard.appendChild(column)
-    }
-    for (let i = 0; i < chosenSize+3; i ++){
-    createColumn(i)
-    gameState.gameBoard[`column${i+1}`] = generateColumns()
-    }
-}
-setGameBoard()
-
-
-const visColumn = document.querySelector('.styleColumn')
-
+// place token click listener
 visBoard.addEventListener(`click`, function(event) {
-
-    const columnClicked = event.target.parentElement.id
-
-    let slotId = null
-    function slotFinder () {
-        for (let i = 1; i < 7; i++) {
-       
-        let slotValue = gameState.gameBoard[columnClicked].get(`slot${i}`)
-
-            if (slotValue === null){
-            gameState.gameBoard[columnClicked].set(`slot${i}`, 'red')
-            slotId = `slot${i}`
-            break;
-            }
-        }
-    }   
-    slotFinder()
-
-    // console.log(gameState.gameBoard, 'gameboard post click')
-   
-    function placeGameToken () {
-        const slot = document.querySelector(`#` +columnClicked+slotId)
-        const token = document.createElement('div')
-        token.classList.add (`playerToken`, `styleToken`)
-        slot.appendChild(token)
+    //value is the ID of the column at the click event
+    // *****************band-aid+++++ revisit later
+    const columnClicked = event.target.parentElement.id.slice(0,7)
+    gameState.placeGameToken(columnClicked)
+    if(gameState.turn === gameState.players[0]){
+        gameState.turn = gameState.players[1]
+    } else {
+        gameState.turn = gameState.players[0]
     }
-    placeGameToken()
+    gameState.checkForWinner()
 });
-
-
 
 
 // function solutionCheck () {
@@ -101,28 +113,6 @@ visBoard.addEventListener(`click`, function(event) {
 // function updateBoard() {
 //     // update board values
 // }
-
-
-
-
-
-
-// function findASlot234() {
-//     let outValue = null
-//     //gameState.gameBoard[columnClicked]
-    
-//     gameState.gameBoard[columnClicked]
-//     console.log(gameState.gameBoard[columnClicked].entries())   
-//     return outValue
-//   }
-
-    // if (visColumn.firstChild.nodeValue !== 'red') {
-    //   t2.firstChild.nodeValue = "two";
-    // } else {
-    //   t2.firstChild.nodeValue = "three";
-    //   controller.abort();
-
-// console.log(Object.defineProperty(gameState.gameBoard, columnClicked, firstFreeSlot {value : gameState.turn}))
 
 
 
