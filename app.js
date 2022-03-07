0b435d4e81c4b1855b8ebd1de67fd31ab476576f
Josh S Wilkinson <joshswilkinson@outlook.com>
2022-03-06 23:36:12 -0600

    basic game fully functional

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 /* Variable naming key
 html'xxx' = static html
-vis'xxx' = procedurally gen html
+vis'xxx' = linked html
 style'xxx' = CSS visual element
 */
 const visBoard = document.querySelector('.htmlGameBoard')
 const dropSelection = document.querySelector('select')
 
 const gameState = {
-    gameBoard: new Map,
+    gameBoard: {},
     connect: 4,
     players: ['red', 'yellow'],
     turn: 'red', // define value
     gameStatus: 'playing',
     setGameBoard: function () {
-       
+
         // creates html elements based on selection
         function createHTMLColumn(counter) {
             let column = document.createElement('div')
@@ -58,6 +58,17 @@ const gameState = {
                     //assigns the slot value according to the active players turn
                     gameState.gameBoard[gameState.lastClickEvent.column].set(`slot${i}`, gameState.turn)
                     gameState.lastClickEvent.slot = `slot${i}`
+
+
+                    //places a token same unassigned slot as slotFinder, *Visually*, assigned to the current player's turn
+                    function generateGameToken() {
+                        const slot = document.querySelector(`#` + gameState.lastClickEvent.column + gameState.lastClickEvent.slot)
+                        const token = document.createElement('div')
+                        token.classList.add(`${gameState.turn}PlayerToken`, `styleToken`)
+                        slot.appendChild(token)
+                    }
+                    // *function call*
+                    generateGameToken()
                     break;
                 }
             }
@@ -65,76 +76,244 @@ const gameState = {
         // *function call*
         slotFinder()
 
-        //places a token same unassigned slot as slotFinder, *Visually*, assigned to the current player's turn
-        function generateGameToken() {
-            const slot = document.querySelector(`#` + gameState.lastClickEvent.column + gameState.lastClickEvent.slot)
-            const token = document.createElement('div')
-            token.classList.add(`${gameState.turn}PlayerToken`, `styleToken`)
-            slot.appendChild(token)
-        }
-        // *function call*
-        generateGameToken()
         // console.log(gameState.gameBoard, 'gameboard post click')
     },
     checkForWinner: function () {
         const solutionCounter = {
             verticalCheck: 0,
-            leftRowCheck: 0,
-            rightRowCheck: 0,
-            lowerLeftDiagCheck: 0,
-            upperLeftDiagCheck: 0,
-            upperRightDiagCheck: 0,
-            lowerLeftDiagCheck: 0
+            leftRowCheck: 1,
+            rightRowCheck: 1,
+            lowerLeftDiagCheck: 1,
+            upperLeftDiagCheck: 1,
+            lowerRightDiagCheck: 1,
+            upperRightDiagCheck: 1
         }
-        
+        const solutionIDXs = {
+            toTheLeftIDX: null,
+            toTheRightIDX: null,
+            verticalIDX: null,
+            verticalIDX2: null
+        }
+        const gameBoardIDX = Array.from(Object.keys(gameState.gameBoard))
         //check vertical win
-            function checkVertical() {
-                for (const entry of gameState.gameBoard[gameState.lastClickEvent.column]) {
-                    if (entry[1] !== gameState.turn) {
-                        break;
-                    } else {
-                        solutionCounter.verticalCheck++
-                        if (solutionCounter.verticalCheck === gameState.connect) {
+
+
+        function checkVertical() {
+            for (const entry of gameState.gameBoard[gameState.lastClickEvent.column]) {
+                if (entry[1] !== gameState.turn) {
+                    break;
+                } else {
+                    solutionCounter.verticalCheck++
+                    if (solutionCounter.verticalCheck === gameState.connect) {
                         // placeholder *** add real output
                         console.log(`${gameState.turn} Wins!`)
-                        }
+                        break;
                     }
                 }
             }
+        }
         //*function call*
         checkVertical()
-        // check to the left
-        function checkLeft (){
-            
+        // checks to the left
+        function checkLeft() {
+            for (let i = 1; i < gameState.connect; i++) {
+                solutionIDXs.toTheLeftIDX = gameBoardIDX.indexOf(gameState.lastClickEvent.column) - i
+                if (solutionIDXs.toTheLeftIDX === -1) {
+                    break
+                }
+                let toTheLeft = gameState.gameBoard[gameBoardIDX[solutionIDXs.toTheLeftIDX]].get(gameState.lastClickEvent.slot)
+                // console.log(toTheLeft)
+                if (toTheLeft !== gameState.turn) {
+                    break
+                } else {
+                    solutionCounter.leftRowCheck++
+                    if (solutionCounter.leftRowCheck === gameState.connect) {
+                        console.log(`${gameState.turn} Wins!`)
+                        break;
+                    }
+                }
 
+            }
         }
         //*function call*
         checkLeft()
-        console.log(gameState.gameBoard.entries())
-        console.log(gameState.lastClickEvent)
 
+        function checkRight() {
+            for (let i = 1; i < gameState.connect; i++) {
+                solutionIDXs.toTheRightIDX = gameBoardIDX.indexOf(gameState.lastClickEvent.column) + i
+                if (solutionIDXs.toTheRightIDX >= gameState.connect + 3) {
+                    break
+                }
+                let toTheRight = gameState.gameBoard[gameBoardIDX[solutionIDXs.toTheRightIDX]].get(gameState.lastClickEvent.slot)
+                if (toTheRight !== gameState.turn) {
+                    break
+                } else {
+                    solutionCounter.rightRowCheck++
+                    if (solutionCounter.rightRowCheck === gameState.connect) {
+                        console.log(`${gameState.turn} Wins!`)
+                        break;
+                    }
+                }
 
+            }
+        }
+        // **function call**
+        checkRight()
 
+        function checkDiagonalLowerLeft() {
+            for (let i = 1; i < gameState.connect; i++) {
+                //column to the left of the last placed token
+                solutionIDXs.toTheLeftIDX = gameBoardIDX.indexOf(gameState.lastClickEvent.column) - i
+                //invalid coordinate check
+                if (solutionIDXs.toTheLeftIDX === -1) {
+                    break
+                }
+                //vertical postion of the last placed token
+                let verticalIdxArray = Array.from(gameState.gameBoard[gameBoardIDX[solutionIDXs.toTheLeftIDX]].keys())
+                // the vertical idx of slot(s) below
+                solutionIDXs.verticalIDX = verticalIdxArray.indexOf(gameState.lastClickEvent.slot) - i
+                //invalid coordinate check
+                if (solutionIDXs.verticalIDX === -1) {
+                    break
+                }
 
+                // Lower check value
+                let toTheLower = gameState.gameBoard[gameBoardIDX[solutionIDXs.toTheLeftIDX]].get(verticalIdxArray[solutionIDXs.verticalIDX])
+                console.log(toTheLower, "to the lower left Diag")
+                // Lower left Win Check
+                if (toTheLower !== gameState.turn) {
+                    break
+                } else {
+                    solutionCounter.lowerLeftDiagCheck++
+                    console.log(solutionCounter.lowerLeftDiagCheck)
+                    if (solutionCounter.lowerLeftDiagCheck === gameState.connect) {
+                        console.log(`${gameState.turn} Wins!`)
+                        break;
+                    }
+                }
 
+            }
+        }
+        // **function call
+        checkDiagonalLowerLeft()
 
+        function checkDiagonalUpperLeft() {
+            for (let i = 1; i < gameState.connect; i++) {
+                //column to the left of the last placed token
+                solutionIDXs.toTheLeftIDX = gameBoardIDX.indexOf(gameState.lastClickEvent.column) - i
+                //invalid coordinate check
+                if (solutionIDXs.toTheLeftIDX === -1) {
+                    break
+                }
+                //vertical postion of the last placed token
+                let verticalIdxArray = Array.from(gameState.gameBoard[gameBoardIDX[solutionIDXs.toTheLeftIDX]].keys())
+                // the vertical idx of slot(s) above
+                solutionIDXs.verticalIDX2 = verticalIdxArray.indexOf(gameState.lastClickEvent.slot) + i
+                //invalid coordinate check
+                if (solutionIDXs.verticalIDX2 > gameState.connect + 3) {
+                    break
+                }
+
+                // Upper check value
+                let toTheUpper = gameState.gameBoard[gameBoardIDX[solutionIDXs.toTheLeftIDX]].get(verticalIdxArray[solutionIDXs.verticalIDX2])
+                // Upper left Win Check
+                if (toTheUpper !== gameState.turn) {
+                    break
+                } else {
+                    solutionCounter.upperLeftDiagCheck++
+                    if (solutionCounter.upperLeftDiagCheck === gameState.connect) {
+                        console.log(`${gameState.turn} Wins!`)
+                        break;
+                    }
+                    // console.log(toTheUpper, 'to the upper left Diag')
+                }
+            }
+        }
+        // ** function call
+        checkDiagonalUpperLeft()
+        // 
+        function checkDiagonalLowerRight() {
+            for (let i = 1; i < gameState.connect; i++) {
+                //column to the left of the last placed token
+                solutionIDXs.toTheRightIDX = gameBoardIDX.indexOf(gameState.lastClickEvent.column) + i
+                
+                //invalid coordinate check  
+                if (solutionIDXs.toTheRightIDX >= gameState.connect + 3) {
+                    break
+                }
+                //vertical postion of the last placed token
+                let verticalIdxArray = Array.from(gameState.gameBoard[gameBoardIDX[solutionIDXs.toTheRightIDX]].keys())
+                // the vertical idx of slot(s) below
+                solutionIDXs.verticalIDX = verticalIdxArray.indexOf(gameState.lastClickEvent.slot) - i
+                //invalid coordinate check
+                if (solutionIDXs.verticalIDX === -1) {
+                    break
+                }
+
+                // Lower check value
+                let toTheLower = gameState.gameBoard[gameBoardIDX[solutionIDXs.toTheRightIDX]].get(verticalIdxArray[solutionIDXs.verticalIDX])
+                console.log(toTheLower, "to the lower right Diag")
+                // Lower Right Win Check
+                if (toTheLower !== gameState.turn) {
+                    break
+                } else {
+                    solutionCounter.lowerRightDiagCheck++
+                    if (solutionCounter.lowerRightDiagCheck === gameState.connect) {
+                        console.log(`${gameState.turn} Wins!`)
+                        break;
+                    }
+                }
+
+            }
+        }
+        //**function call
+        checkDiagonalLowerRight()
+
+        function checkDiagonalUpperRight() {
+            for (let i = 1; i < gameState.connect; i++) {
+                //column to the left of the last placed token
+                solutionIDXs.toTheRightIDX = gameBoardIDX.indexOf(gameState.lastClickEvent.column) + i
+                //invalid coordinate check
+                if (solutionIDXs.toTheRightIDX > gameState.connect + 2) {
+                    break
+                }
+                //vertical postion of the last placed token
+                let verticalIdxArray = Array.from(gameState.gameBoard[gameBoardIDX[solutionIDXs.toTheRightIDX]].keys())
+                // the vertical idx of slot(s) above
+                solutionIDXs.verticalIDX2 = verticalIdxArray.indexOf(gameState.lastClickEvent.slot) + i
+                //invalid coordinate check
+                if (solutionIDXs.verticalIDX2 >= gameState.connect + 3) {
+                    break
+                }
+
+                // Upper check value
+                let toTheUpper = gameState.gameBoard[gameBoardIDX[solutionIDXs.toTheRightIDX]].get(verticalIdxArray[solutionIDXs.verticalIDX2])
+                // Upper Right Win Check
+                if (toTheUpper !== gameState.turn) {
+                    break
+                } else {
+                    solutionCounter.upperRightDiagCheck++
+                    if (solutionCounter.upperRightDiagCheck === gameState.connect) {
+                        console.log(`${gameState.turn} Wins!`)
+                        break;
+                    }
+                    // console.log(toTheUpper, 'to the upper Right Diag')
+                }
+            }
+        }
+        // ** function call
+        checkDiagonalUpperRight()
     }
-
 }
-
-
-
-
-
-console.log(gameState.gameBoard)
+// console.log(gameState.gameBoard)
 
 // **** Set Game Size ****
 //dropdown game-size listener
 dropSelection.addEventListener("change", function (event) {
-       gameState.connect = Number(event.target.value)
+        gameState.connect = Number(event.target.value)
     }),
-// function call to set game size
-gameState.setGameBoard()
+    // function call to set game size
+    gameState.setGameBoard()
 
 // place token click listener
 visBoard.addEventListener(`click`, async function (event) {
@@ -148,4 +327,4 @@ visBoard.addEventListener(`click`, async function (event) {
     } else {
         gameState.turn = gameState.players[0]
     }
-});
+})
---
+++ testing.js
@@ -0,0 +1,9 @@
+const testMap = new Map ([
+    ['slot1', 'hi1'],
+    ['slot2', 'hi2'],
+    ['slot3', 'hi3'],
+    ['slot4', 'hi4']
+])
+
+let iterator = testMap.entries()
+console.log(iterator, 'testing')
